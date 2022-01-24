291d84a17bbe06581f7fb9adfe97281a1dee3074
lisa <lisa@ilark.co.kr>
2022-01-24 16:26:10 +0900

    hi

--- app/Models/Client/Client.php
+++ app/Models/Client/Client.php
@@ -5,13 +5,14 @@ namespace App\Models\Client;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Client extends Model  //'Client'는 테이블명.
+class Client extends Model  //'Client'는 모델명.
 {
-    use HasFactory;         // Class 상속같은 개념.
+    use HasFactory;         // php의 Class 상속같은 개념. (위랑 같은)
 
     protected $fillable = [
         'content',
         'title',
     ];
+
 
 }
--- app/Models/Client/ClientPlayer.php
+++ app/Models/Client/ClientPlayer.php
@@ -5,7 +5,14 @@ namespace App\Models\Client;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ClientPlayer extends Model
+
+class ClientPlayer extends Model    // Model 'Client'와 'Player'을 연결한 맵핑테이블이라는 뜻.
 {
     use HasFactory;
+
+    protected $table = 'client_player';
+    // 원래는 테이블명이 'client_players'로 자동생성 되는데,
+    // 맵핑 테이블이기 때문에 뒤에 s를 뺀 'client_player'로 이름을 지정해줌.
+    // 지정을 안해주면, 나중에 테이블을 생성할 때 못 찾음.
+
 }
